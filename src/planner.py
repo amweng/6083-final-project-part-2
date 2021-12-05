c55061a0bb2fb5cb9d7e06a542a2f3fc98c151a1
rw2873 <rw2873@jedi.poly.edu>
2021-12-05 00:12:36 +0000

    Established State for Planner. Queried All Events for Planner/View subpage.

--- src/planner.py
+++ src/planner.py
@@ -1,28 +1,48 @@
 import streamlit as st
 import functions
-from enum import Enum
+import pandas
 
-user = 0
-action = 0
-    
+user = 'test'
+action = 'test'
+userName = 'test'
 
-def UI_selectAction():
-    aEventPlannerActions = ["Make a New Event", "View Your Events", "Book Resources", "Cancel an Event"]
-    page = st.sidebar.selectbox("What do you want to do?", aEventPlannerActions)
+def getUserID(userName: str, users: pandas.DataFrame):
+    dfUserRow = users[users.name.isin([userName])]
+    aUserEmail = dfUserRow['email'].tolist()[0]
+    return aUserEmail
 
-def UI_Display():
-    UI_selectEP
-
-def makeEvent():
-    pass
-
+def getUserName(userEmail: str, users: pandas.DataFrame):
+    dfUserRow = user[user.email.isin([userEmail])]
+    aUserName = dfUserRow['name'].tolist()[0]
+    return aUserName
 
 def show():
-    qEventPlanners = "SELECT first_name, last_name FROM event_planners;"
+    qEventPlanners = "SELECT email, first_name, last_name FROM event_planners;"
     dfEventPlannerNames = functions.query_db(qEventPlanners)
-    dfEventPlannerNames['name'] = (dfEventPlannerNames['first_name'] + ' ' + dfEventPlannerNames['last_name'])
+    dfEventPlannerNames['name'] = (dfEventPlannerNames['first_name'] \
+        + ' ' + dfEventPlannerNames['last_name'] + " (" + dfEventPlannerNames['email'] + ')')
     aEventPlannerNames = dfEventPlannerNames['name'].tolist()
     user = st.sidebar.selectbox("Who are you?", aEventPlannerNames)
+    userName = user
 
-    st.write(user)
+    aEventPlannerActions = ["Make a New Event", "View Your Events", "Book Resources", "Cancel an Event"]
+    action = st.sidebar.selectbox("What do you want to do?", aEventPlannerActions)
+
+    user = getUserID(user, dfEventPlannerNames)
+
+    if(action == "Make a New Event"):
+        pass
+    elif(action == "View Your Events"):
+        qFetchEvents = "SELECT * FROM events WHERE event_planner_email LIKE '" + user + \
+            "' ORDER BY date, start_at;"
+        dfEventsAll = functions.query_db(qFetchEvents)
+        
+        # Give the user a quick glance of all their created events
+        st.markdown("## Events planned by " + userName + ":")
+        dfEventsSurvey = dfEventsAll[['date', 'start_at', 'end_at', 'event_name']]
+        st.write(dfEventsSurvey)
+    elif(action == "Book Resources"):
+        pass
+    elif(action == "Cancel an Event"):
+        pass
     
